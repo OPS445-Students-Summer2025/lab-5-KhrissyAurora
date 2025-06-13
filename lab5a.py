54ed87071ed3ffc21fe6e438d62b500979769aec
Kris Sadiasa <kalmodal@myseneca.ca>
2025-06-13 06:18:54 -0400

    Adding lab5a.py

---
+++ lab5a.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# Author ID: 114163223
+
+def read_file_string(file_name):
+    # Takes file_name as string for a file name, returns its entire contents as a string
+    try:
+        with open(file_name, 'r') as f:
+            return f.read()
+    except Exception as e:
+        return f"Error reading file: {e}"
+
+def read_file_list(file_name):
+    # Takes a file_name as string for a file name,
+    # returns its entire contents as a list of lines without new-line characters
+    try:
+        with open(file_name, 'r') as f:
+            return [line.strip() for line in f.readlines()]
+    except Exception as e:
+        return [f"Error reading file: {e}"]
+
+if __name__ == '__main__':
+    file_name = 'data.txt'
+    print(read_file_string(file_name))
+    print(read_file_list(file_name))
